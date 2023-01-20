d184a4d2e382f8ee0b58912c3abf780c0b65cf8c
exb5429 <exb5429@psu.edu>
2023-01-20 09:03:10 -0500

    Lab2 Commit

--- src/lab2/cities_n_routes.py
+++ src/lab2/cities_n_routes.py
@@ -12,13 +12,18 @@ def get_randomly_spread_cities(size, n_cities):
     > This function takes in the size of the map and the number of cities to be generated 
     and returns a list of cities with their x and y coordinates. The cities are randomly spread
     across the map.
-    
+
     :param size: the size of the map as a tuple of 2 integers
     :param n_cities: The number of cities to generate
     :return: A list of cities with random x and y coordinates.
     """
+    cityList = []
+    for i in range(n_cities): 
+        cityList.append((random.randint(0,size[0]),random.randint(0,size[1])))
+        
+
     # Consider the condition where x size and y size are different
-    pass
+    return cityList
 
 def get_routes(city_names):
     """
@@ -29,17 +34,20 @@ def get_routes(city_names):
     :return: A list of tuples representing all possible links between cities/ pairs of cities, 
             each item in the list (a link) represents a route between two cities.
     """
-    pass
+
+    combinations = itertools.combinations(city_names,2)
+
+    return combinations
 
 
 # TODO: Fix variable names
 if __name__ == '__main__':
     city_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     '''print the cities and routes'''
-    cities = get_randomly_spread_cities((100, 200), len(city_names))
+    cityCoords = get_randomly_spread_cities((100, 200), len(city_names))
     routes = get_routes(city_names)
     print('Cities:')
-    for i, city in enumerate(cities):
+    for i, city in enumerate(cityCoords):
         print(f'{city_names[i]}: {city}')
     print('Routes:')
     for i, route in enumerate(routes):
