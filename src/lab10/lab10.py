1dd8d0813d2be6a3a0c62cdd500b023e271d6415
exb5429 <exb5429@psu.edu>
2023-03-17 09:02:32 -0400

    Update lab10.py
    KNeighbors heart disease

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -10,7 +10,8 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -26,10 +27,26 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
-sklearn_model = None
+sklearn_model = KNeighborsClassifier(n_neighbors= 8)
+sklearn_model.fit(x_train, y_train)
+
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
+
+
+df_max_scaled = df.copy()
+for column in df_max_scaled.columns:
+    df_max_scaled[column] = df_max_scaled[column]  / df_max_scaled[column].abs().max()
+
+y = df_max_scaled.HeartDisease.values
+x = df_max_scaled.drop(["HeartDisease"], axis=1)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=25
+)
+
+sklearn_model = KNeighborsClassifier(n_neighbors= 8)
+sklearn_model.fit(x_train, y_train)
 
 
 """ Improve the model by normalizing the input data. """
